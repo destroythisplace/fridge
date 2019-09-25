1107749473deafdd5e45941d1fa03de2e9cf5e25
imgoodthanks <moremike@protonmail.com>
2019-09-25 21:24:18 +0000

    Create project.

---
+++ lib/fridge.ex
@@ -0,0 +1,18 @@
+defmodule Fridge do
+  @moduledoc """
+  Documentation for Fridge.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fridge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
